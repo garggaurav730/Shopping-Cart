e49e23f16d225e01a4ef4db9040b861d7436fd6d
Gaurav Garg <gaugarg4@publicisgroupe.net>
2022-07-06 14:46:25 +0530

    products page completed

--- products.js
+++ products.js
@@ -28,3 +28,34 @@ fetch("categories.json")
   .catch((err) => {
     console.log(err.message);
   });
+
+// Display products
+fetch("products.json")
+  .then((res) => res.json())
+  .then((products) => {
+    const products_list = products
+      .map(
+        (product) => `<li class="cards-item">
+    <h4 class="card-header">${product.name}</h4>
+    <div class="card">
+      <div class="card-top">
+        <div class="card-image"><img src="${product.imageURL}" alt="${product.name}" class="card-img"></div>
+      </div>
+      <div class="card-content">
+        <p class="card-text">${product.description}</p>
+        <div class="card-footer">
+          <p class="card-footer-left">MRP Rs.<span class="prod-price">${product.price}</span></p>
+          <button class="btn card-btn" onclick="addToCart('${product.id}')">Buy Now</button>
+          <button class="btn card-btn-sm" onclick="addToCart('${product.id}')">Buy Now @ Rs.${product.price}</button>
+        </div>
+      </div>
+    </div>
+    <button class="btn card-btn-md" onclick="addToCart('${product.id}')">Buy Now @ Rs.${product.price}</button>
+  </li>`
+      )
+      .join("");
+    document.querySelector(".cards").innerHTML = products_list;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
